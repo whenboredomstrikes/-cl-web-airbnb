961d76488fa70bde90e309e277123a555f116409
Kurup <92325652+whenboredomstrikes@users.noreply.github.com>
2023-10-05 17:21:35 +0530

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -15,11 +15,11 @@ export default function Card(props) {
                 <div className="card--badge">{badgeText}</div>
             }
             <img 
-                src={`../airbnb-clone/images/${props.coverImg}`} 
+                src={`../images/${props.coverImg}`} 
                 className="card--image" 
             />
             <div className="card--stats">
-                <img src="../airbnb-clone/images/star.png" className="card--star" />
+                <img src="../images/star.png" className="card--star" />
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
